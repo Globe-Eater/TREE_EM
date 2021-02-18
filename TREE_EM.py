0e33a0eca8915a7f527da0fc45bf1aab307f51c2
Globe-Eater <bullokl@ostatemail.okstate.edu>
2021-02-18 16:21:26 -0600

    Added Tables to shell interface.

--- TREE_EM.py
+++ TREE_EM.py
@@ -3,14 +3,14 @@ import sys
 import click
 
 from app import create_app, db
-# from app.models import User, Permission, Role, Images
+from app.models import User, Permission, Role, Image
 
 # Flask Coverage
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
-#@app.shell_context_processor
-# def make_shell_context():
-#     return dict(db=db, User=User, Permission=Permission, Role=Role)
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Permission=Permission, Role=Role)
 
 
